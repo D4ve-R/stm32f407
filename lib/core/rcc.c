eba55718213d44ab5cc4f46ac58244f954b555a4
David R <david.rechkemmer@googlemail.com>
2022-03-24 23:19:11 +0100

    stupid

--- lib/core/rcc.c
+++ lib/core/rcc.c
@@ -6,66 +6,34 @@
 
 #include <rcc.h>
 
+void _RCC_SetRegister(volatile uint32_t *reg, uint8_t port){
+	*reg |= (1 << port);
+}
+
 void RCC_EnableAHB1(uint8_t port){
 	RCC_Handle_t rcc;
 	rcc.pRCC = RCC;
-	switch(port){
-			case RCC_PORTA:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTA);
-				break;
-			case RCC_PORTB:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTB);
-				break;
-			case RCC_PORTC:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTC);
-				break;
-			case RCC_PORTD:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTD);
-				break;
-			case RCC_PORTE:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTE);
-				break;
-			case RCC_PORTF:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTF);
-				break;
-			case RCC_PORTG:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTG);
-				break;
-			case RCC_PORTH:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTH);
-				break;
-			case RCC_PORTI:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTI);
-				break;
-			case RCC_PORTJ:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTJ);
-				break;
-			case RCC_PORTK:
-				rcc.pRCC->AHB1ENR |= (1 << RCC_PORTK);
-				break;
-			default:
-				break;
-		}
+	//uint32_t *reg = &rcc.pRCC->AHB1ENR;
+	_RCC_SetRegister(&rcc.pRCC->AHB1ENR, port);
 }
 
 void RCC_DisableAHB1(uint8_t port){
-	return;
+	RCC_Handle_t rcc;
+	rcc.pRCC = RCC;
+	//uint32_t *reg = &rcc.pRCC->AHB1RSTR;
+	_RCC_SetRegister(&rcc.pRCC->AHB1RSTR, port);
 }
 
 void RCC_EnableAPB2(uint8_t port){
 	RCC_Handle_t rcc;
 	rcc.pRCC = RCC;
-	switch(port){
-		case RCC_SYSCFG:
-			rcc.pRCC->APB2ENR |= (1 << RCC_SYSCFG);
-			break;
-		default:
-			break;
-	}
+	_RCC_SetRegister(&rcc.pRCC->APB2ENR, port);
 
 }
 
-void RCC_DisableAPB2(){
-
+void RCC_DisableAPB2(uint8_t port){
+	RCC_Handle_t rcc;
+	rcc.pRCC = RCC;
+	_RCC_SetRegister(&rcc.pRCC->APB2RSTR, port);
 }
 
